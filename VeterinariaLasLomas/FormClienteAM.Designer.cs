5204ca8bf4ad9554acf348fb2ebc2d413934dde0
Leonardo <leocueto1999@gmail.com>
2026-07-10 22:41:24 -0300

    feature: add BLLCliente

---
+++ VeterinariaLasLomas/FormClienteAM.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace VeterinariaLasLomas
+{
+    partial class FormClienteAM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // FormClienteAM
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(460, 450);
+            Name = "FormClienteAM";
+            Text = "FormClienteAM";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
