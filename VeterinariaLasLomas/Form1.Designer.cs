02fb01ba976335ba9bb4429cc30a49035765ebad
Leonardo <leocueto1999@gmail.com>
2026-07-11 21:43:16 -0300

    feat: implement customer CRUD with DTOs and UI improvements
    * Add DTOCliente to expose customer data to the presentation layer.
    * Add GetById and improve exception handling in BLLCliente.
    * Improve the customer management UI in Form1, including create, delete, update, refresh, and active-customer filtering.
    * Add FormClienteAM for creating and editing customers, with validations and preloaded data.

--- VeterinariaLasLomas/Form1.Designer.cs
+++ VeterinariaLasLomas/Form1.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle10 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle11 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle12 = new DataGridViewCellStyle();
             tabControl = new TabControl();
             tabTurnos = new TabPage();
             tabHistorial = new TabPage();
@@ -64,31 +64,28 @@
             tabControl.Controls.Add(tabMascotas);
             tabControl.Controls.Add(tabVeterinarios);
             tabControl.Font = new Font("Segoe UI", 9F);
-            tabControl.Location = new Point(6, 3);
-            tabControl.Margin = new Padding(3, 4, 3, 4);
+            tabControl.Location = new Point(5, 2);
             tabControl.Name = "tabControl";
             tabControl.SelectedIndex = 0;
-            tabControl.Size = new Size(909, 596);
+            tabControl.Size = new Size(795, 447);
             tabControl.TabIndex = 0;
             // 
             // tabTurnos
             // 
-            tabTurnos.Location = new Point(4, 29);
-            tabTurnos.Margin = new Padding(3, 4, 3, 4);
+            tabTurnos.Location = new Point(4, 24);
             tabTurnos.Name = "tabTurnos";
-            tabTurnos.Padding = new Padding(3, 4, 3, 4);
-            tabTurnos.Size = new Size(901, 563);
+            tabTurnos.Padding = new Padding(3);
+            tabTurnos.Size = new Size(787, 419);
             tabTurnos.TabIndex = 0;
             tabTurnos.Text = "Turnos";
             tabTurnos.UseVisualStyleBackColor = true;
             // 
             // tabHistorial
             // 
-            tabHistorial.Location = new Point(4, 29);
-            tabHistorial.Margin = new Padding(3, 4, 3, 4);
+            tabHistorial.Location = new Point(4, 24);
             tabHistorial.Name = "tabHistorial";
-            tabHistorial.Padding = new Padding(3, 4, 3, 4);
-            tabHistorial.Size = new Size(901, 563);
+            tabHistorial.Padding = new Padding(3);
+            tabHistorial.Size = new Size(787, 419);
             tabHistorial.TabIndex = 1;
             tabHistorial.Text = "Historial";
             tabHistorial.UseVisualStyleBackColor = true;
@@ -101,11 +98,10 @@
             tabClientes.Controls.Add(btnBajaCliente);
             tabClientes.Controls.Add(btnNuevoCliente);
             tabClientes.Controls.Add(dgvClientes);
-            tabClientes.Location = new Point(4, 29);
-            tabClientes.Margin = new Padding(3, 4, 3, 4);
+            tabClientes.Location = new Point(4, 24);
             tabClientes.Name = "tabClientes";
-            tabClientes.Padding = new Padding(3, 4, 3, 4);
-            tabClientes.Size = new Size(901, 563);
+            tabClientes.Padding = new Padding(3);
+            tabClientes.Size = new Size(787, 419);
             tabClientes.TabIndex = 2;
             tabClientes.Text = "Clientes";
             tabClientes.UseVisualStyleBackColor = true;
@@ -113,55 +109,61 @@
             // btnActualizarClientes
             // 
             btnActualizarClientes.BackColor = Color.Transparent;
-            btnActualizarClientes.Location = new Point(664, 45);
-            btnActualizarClientes.Margin = new Padding(3, 4, 3, 4);
+            btnActualizarClientes.Cursor = Cursors.Hand;
+            btnActualizarClientes.Location = new Point(581, 34);
             btnActualizarClientes.Name = "btnActualizarClientes";
-            btnActualizarClientes.Size = new Size(109, 37);
+            btnActualizarClientes.Size = new Size(95, 28);
             btnActualizarClientes.TabIndex = 5;
             btnActualizarClientes.Text = "Actualizar";
             btnActualizarClientes.UseVisualStyleBackColor = false;
+            btnActualizarClientes.Click += btnActualizarClientes_Click;
             // 
             // btnModificarCliente
             // 
             btnModificarCliente.BackColor = Color.Transparent;
-            btnModificarCliente.Location = new Point(279, 25);
-            btnModificarCliente.Margin = new Padding(3, 4, 3, 4);
+            btnModificarCliente.Cursor = Cursors.Hand;
+            btnModificarCliente.Location = new Point(244, 19);
             btnModificarCliente.Name = "btnModificarCliente";
-            btnModificarCliente.Size = new Size(129, 57);
+            btnModificarCliente.Size = new Size(113, 43);
             btnModificarCliente.TabIndex = 4;
             btnModificarCliente.Text = "Modificar cliente";
             btnModificarCliente.UseVisualStyleBackColor = false;
+            btnModificarCliente.Click += btnModificarCliente_Click;
             // 
             // chkActivos
             // 
             chkActivos.AutoSize = true;
-            chkActivos.Location = new Point(795, 57);
-            chkActivos.Margin = new Padding(3, 4, 3, 4);
+            chkActivos.Checked = true;
+            chkActivos.CheckState = CheckState.Checked;
+            chkActivos.Cursor = Cursors.Hand;
+            chkActivos.Location = new Point(696, 43);
             chkActivos.Name = "chkActivos";
-            chkActivos.Size = new Size(111, 24);
+            chkActivos.Size = new Size(89, 19);
             chkActivos.TabIndex = 3;
             chkActivos.Text = "Solo activos";
             chkActivos.UseVisualStyleBackColor = true;
+            chkActivos.CheckedChanged += chkActivos_CheckedChanged;
             // 
             // btnBajaCliente
             // 
-            btnBajaCliente.Location = new Point(143, 25);
-            btnBajaCliente.Margin = new Padding(3, 4, 3, 4);
+            btnBajaCliente.Cursor = Cursors.Hand;
+            btnBajaCliente.Location = new Point(125, 19);
             btnBajaCliente.Name = "btnBajaCliente";
-            btnBajaCliente.Size = new Size(129, 57);
+            btnBajaCliente.Size = new Size(113, 43);
             btnBajaCliente.TabIndex = 2;
             btnBajaCliente.Text = "Dar de baja";
             btnBajaCliente.UseVisualStyleBackColor = true;
+            btnBajaCliente.Click += btnBajaCliente_Click;
             // 
             // btnNuevoCliente
             // 
             btnNuevoCliente.BackColor = Color.Transparent;
-            btnNuevoCliente.Location = new Point(7, 25);
-            btnNuevoCliente.Margin = new Padding(3, 4, 3, 4);
+            btnNuevoCliente.Cursor = Cursors.Hand;
+            btnNuevoCliente.Location = new Point(6, 19);
             btnNuevoCliente.Name = "btnNuevoCliente";
-            btnNuevoCliente.Size = new Size(129, 57);
+            btnNuevoCliente.Size = new Size(113, 43);
             btnNuevoCliente.TabIndex = 1;
-            btnNuevoCliente.Text = "Nuevo cliente";
+            btnNuevoCliente.Text = "+ Nuevo cliente";
             btnNuevoCliente.UseVisualStyleBackColor = false;
             btnNuevoCliente.Click += btnNuevoCliente_Click;
             // 
@@ -169,39 +171,38 @@
             // 
             dgvClientes.AllowUserToAddRows = false;
             dgvClientes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = SystemColors.Control;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            dgvClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = SystemColors.Control;
+            dataGridViewCellStyle7.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle7.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
+            dgvClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
             dgvClientes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = SystemColors.Window;
-            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
-            dgvClientes.DefaultCellStyle = dataGridViewCellStyle2;
-            dgvClientes.Location = new Point(7, 109);
-            dgvClientes.Margin = new Padding(3, 4, 3, 4);
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = SystemColors.Window;
+            dataGridViewCellStyle8.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle8.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = DataGridViewTriState.False;
+            dgvClientes.DefaultCellStyle = dataGridViewCellStyle8;
+            dgvClientes.Location = new Point(6, 82);
             dgvClientes.MultiSelect = false;
             dgvClientes.Name = "dgvClientes";
             dgvClientes.ReadOnly = true;
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = SystemColors.Control;
-            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
-            dgvClientes.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = SystemColors.Control;
+            dataGridViewCellStyle9.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle9.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = DataGridViewTriState.True;
+            dgvClientes.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
             dgvClientes.RowHeadersWidth = 51;
             dgvClientes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvClientes.Size = new Size(889, 425);
+            dgvClientes.Size = new Size(778, 319);
             dgvClientes.TabIndex = 0;
             // 
             // tabMascotas
@@ -209,54 +210,55 @@
             tabMascotas.Controls.Add(dgvMascotas);
             tabMascotas.Controls.Add(btnBajaMascota);
             tabMascotas.Controls.Add(btnNuevaMascota);
-            tabMascotas.Location = new Point(4, 29);
-            tabMascotas.Margin = new Padding(3, 4, 3, 4);
+            tabMascotas.Location = new Point(4, 24);
             tabMascotas.Name = "tabMascotas";
-            tabMascotas.Padding = new Padding(3, 4, 3, 4);
-            tabMascotas.Size = new Size(901, 563);
+            tabMascotas.Padding = new Padding(3);
+            tabMascotas.Size = new Size(787, 419);
             tabMascotas.TabIndex = 3;
             tabMascotas.Text = "Mascotas";
             tabMascotas.UseVisualStyleBackColor = true;
             // 
             // dgvMascotas
             // 
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = SystemColors.Control;
-            dataGridViewCellStyle4.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            dgvMascotas.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle10.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = SystemColors.Control;
+            dataGridViewCellStyle10.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle10.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = DataGridViewTriState.True;
+            dgvMascotas.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
             dgvMascotas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = SystemColors.Window;
-            dataGridViewCellStyle5.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle5.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.False;
-            dgvMascotas.DefaultCellStyle = dataGridViewCellStyle5;
-            dgvMascotas.Location = new Point(15, 118);
+            dataGridViewCellStyle11.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.BackColor = SystemColors.Window;
+            dataGridViewCellStyle11.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle11.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle11.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle11.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = DataGridViewTriState.False;
+            dgvMascotas.DefaultCellStyle = dataGridViewCellStyle11;
+            dgvMascotas.Location = new Point(13, 88);
+            dgvMascotas.Margin = new Padding(3, 2, 3, 2);
             dgvMascotas.Name = "dgvMascotas";
-            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = SystemColors.Control;
-            dataGridViewCellStyle6.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle6.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle6.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.True;
-            dgvMascotas.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle12.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = SystemColors.Control;
+            dataGridViewCellStyle12.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle12.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle12.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle12.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = DataGridViewTriState.True;
+            dgvMascotas.RowHeadersDefaultCellStyle = dataGridViewCellStyle12;
             dgvMascotas.RowHeadersWidth = 51;
-            dgvMascotas.Size = new Size(877, 438);
+            dgvMascotas.Size = new Size(767, 328);
             dgvMascotas.TabIndex = 2;
             dgvMascotas.CellContentClick += dgvMascotas_CellContentClick;
             // 
             // btnBajaMascota
             // 
-            btnBajaMascota.Location = new Point(207, 41);
+            btnBajaMascota.Location = new Point(181, 31);
+            btnBajaMascota.Margin = new Padding(3, 2, 3, 2);
             btnBajaMascota.Name = "btnBajaMascota";
-            btnBajaMascota.Size = new Size(170, 51);
+            btnBajaMascota.Size = new Size(149, 38);
             btnBajaMascota.TabIndex = 1;
             btnBajaMascota.Text = "Dar de baja";
             btnBajaMascota.UseVisualStyleBackColor = true;
@@ -264,9 +266,10 @@
             // 
             // btnNuevaMascota
             // 
-            btnNuevaMascota.Location = new Point(15, 41);
+            btnNuevaMascota.Location = new Point(13, 31);
+            btnNuevaMascota.Margin = new Padding(3, 2, 3, 2);
             btnNuevaMascota.Name = "btnNuevaMascota";
-            btnNuevaMascota.Size = new Size(186, 51);
+            btnNuevaMascota.Size = new Size(163, 38);
             btnNuevaMascota.TabIndex = 0;
             btnNuevaMascota.Text = "+ Nueva Mascota";
             btnNuevaMascota.UseVisualStyleBackColor = true;
@@ -274,22 +277,20 @@
             // 
             // tabVeterinarios
             // 
-            tabVeterinarios.Location = new Point(4, 29);
-            tabVeterinarios.Margin = new Padding(3, 4, 3, 4);
+            tabVeterinarios.Location = new Point(4, 24);
             tabVeterinarios.Name = "tabVeterinarios";
-            tabVeterinarios.Padding = new Padding(3, 4, 3, 4);
-            tabVeterinarios.Size = new Size(901, 563);
+            tabVeterinarios.Padding = new Padding(3);
+            tabVeterinarios.Size = new Size(787, 419);
             tabVeterinarios.TabIndex = 4;
             tabVeterinarios.Text = "Veterinarios";
             tabVeterinarios.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(800, 450);
             Controls.Add(tabControl);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
--- VeterinariaLasLomas/FormClienteAM.cs
+++ VeterinariaLasLomas/FormClienteAM.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BE;
+using BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +12,70 @@ namespace VeterinariaLasLomas
 {
     public partial class FormClienteAM : Form
     {
+        private BLLCliente bllCliente = new BLLCliente();
+        private BECliente clienteEditar;
         public FormClienteAM()
         {
             InitializeComponent();
+            clienteEditar = null;
+            this.Text = "Nuevo cliente";
+        }
+        public FormClienteAM(BECliente cliente)
+        {
+            InitializeComponent();
+            clienteEditar = cliente;
+            this.Text = "Modificar cliente";
+            PrecargarDatos();
+        }
+        private void PrecargarDatos()
+        {
+            txtNombre.Text = clienteEditar.Nombre;
+            txtApellido.Text = clienteEditar.Apellido;
+            txtDni.Text = clienteEditar.Dni;
+            txtTelefono.Text = clienteEditar.Telefono;
+            txtEmail.Text = clienteEditar.Email;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (clienteEditar == null)
+                {
+                    BECliente nuevo = new BECliente();
+                    nuevo.Nombre = txtNombre.Text;
+                    nuevo.Apellido = txtApellido.Text;
+                    nuevo.Dni = txtDni.Text;
+                    nuevo.Telefono = txtTelefono.Text;
+                    nuevo.Email = txtEmail.Text;
+
+                    bllCliente.Guardar(nuevo);
+                    MessageBox.Show("Cliente guardado correctamente.");
+                }
+                else
+                {
+                    clienteEditar.Nombre = txtNombre.Text;
+                    clienteEditar.Apellido = txtApellido.Text;
+                    clienteEditar.Dni = txtDni.Text;
+                    clienteEditar.Telefono = txtTelefono.Text;
+                    clienteEditar.Email = txtEmail.Text;
+
+                    bllCliente.Modificar(clienteEditar);
+                    MessageBox.Show("Cliente modificado correctamente.");
+                }
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
